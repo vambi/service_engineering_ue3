d61bf7b8af58aa41071eee17d60e9495684fc40a
vambi <simon.vamberszky@live.de>
2019-06-16 13:10:27 +0200

    fixes

--- veranstaltungsservice_angular/service/src/app/app.component.ts
+++ veranstaltungsservice_angular/service/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpServiceService } from './http-service.service';
+import {MessageService} from './message.service';
+
 
 
 @Component({
@@ -10,12 +13,33 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
     title = 'Veranstaltungsservice';
 
+ /**
+   * An object representing the user for the login form
+   */
+  public user: any;
 
-    constructor(private http: HttpClient) { }
 
-    ngOnInit() {
+    constructor(private _userService: HttpServiceService, public MessageService:MessageService) { }
+ 
+  ngOnInit() {
+    this.user = {
+      username: '',
+      password: ''
+    };
+  }
 
-    }
+  login() {
+    this._userService.login({'username': this.user.username, 'password': this.user.password});
+  }
+ 
+  refreshToken() {
+    this._userService.refreshToken();
+  }
+ 
+  logout() {
+    this._userService.logout();
+  }
+    
 
 
 }
--- veranstaltungsservice_angular/service/src/app/participatn/participatn.component.ts
+++ veranstaltungsservice_angular/service/src/app/participatn/participatn.component.ts
@@ -4,6 +4,7 @@ import { Veranstaltung } from '../veranstaltung.interface';
 import { Anmelden } from '../anmelden.interface';
 
 
+
 @Component({
   selector: 'app-participatn',
   templateUrl: './participatn.component.html',
@@ -12,6 +13,11 @@ import { Anmelden } from '../anmelden.interface';
 export class ParticipatnComponent implements OnInit {
 
   events: Veranstaltung[];
+  vorname: string;
+  nachname: string;
+  veranstaltung: number;
+
+
 
   constructor(private service:HttpServiceService) { }
     
@@ -30,8 +36,9 @@ export class ParticipatnComponent implements OnInit {
 }
 
 // Anlegen einer Anmeldung
-onClick(vorname: string, nachname: string, veranstaltung: number) {
+/*onClick(vorname: string, nachname: string, veranstaltung: number) {
   console.log(vorname, nachname, veranstaltung);
+ 
   this.service.registerEvent({ vorname, nachname, veranstaltung })
       .subscribe(
           (data: Anmelden) => {
@@ -41,6 +48,19 @@ onClick(vorname: string, nachname: string, veranstaltung: number) {
           },
           (error: any) => console.log(error)
       );
-}
+}*/
 
+onClick(){
+	this.service.registerEvent({vorname: this.vorname, nachname: this.nachname, veranstaltung: this.veranstaltung}).subscribe(
+		(data: Anmelden) => {
+			window.alert('Anmeldung duchgeführt');
+			this.vorname = '';
+			this.nachname ='';
+			this.veranstaltung = 0;
+			console.log('eingefügt: ',data);
+		},
+		(error:any) => console.log(error)
+	);
+
+}
 }
